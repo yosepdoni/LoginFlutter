dfbb622eb165c18b7c9dc40f8a2f286bda1eea4d
yosepdoni <yosepdoni2905@gmail.com>
2023-03-28 01:11:08 +0700

    hilangin username dan password

--- lib/login.php
+++ lib/login.php
@@ -1,12 +1,8 @@
 <?php
 include 'config.php';
-//for running
+
 $username = $_POST['username'];
 $password = $_POST['password'];
-
-// $username = 'yosep';
-// $password = '2905';
-
 
 $queryResult=$conn->query("SELECT * FROM user WHERE username='".$username."' AND password='".$password."'");
 $result=array();
